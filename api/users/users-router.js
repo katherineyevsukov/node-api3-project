b9d3506050b1498be5937b69d60f34898b509b79
Kat <kyevsukov@gmail.com>
2021-12-01 19:05:07 -0500

    can get user posts

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,51 +1,59 @@
-const express = require('express');
-const User = require('./users-model')
-const Post = require('./../posts/posts-model')
-const { validateUserId, validateUser } = require('./../middleware/middleware')
+const express = require("express");
+const User = require("./users-model");
+const Post = require("./../posts/posts-model");
+const { validateUserId, validateUser } = require("./../middleware/middleware");
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const users = await User.get()
-    res.status(200).json(users)
-  } catch(err){
-    next(err)
+    const users = await User.get();
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
   }
 });
 
-router.get('/:id', validateUserId, (req, res, next) => {
-  res.status(200).json(req.user)
+router.get("/:id", validateUserId, (req, res, next) => {
+  res.status(200).json(req.user);
 });
 
-router.post('/', validateUser, (req, res, next) => {
+router.post("/", validateUser, (req, res, next) => {
   User.insert(req.body)
-  .then(user => {
-    res.status(201).json(user)
-  }).catch(next)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch(next);
 });
 
-router.put('/:id', validateUser, validateUserId, (req, res, next) => {
+router.put("/:id", validateUser, validateUserId, (req, res, next) => {
   User.update(req.params.id, req.body)
-  .then(updated => {
-    res.status(200).json(updated)
-  }).catch(next)
+    .then((updated) => {
+      res.status(200).json(updated);
+    })
+    .catch(next);
 });
 
-router.delete('/:id', validateUserId, (req, res, next) => {
+router.delete("/:id", validateUserId, (req, res, next) => {
   User.remove(req.params.id)
-  .then(number => {
-    number === 1 ? res.status(200).json(req.user) :
-    res.send(`${number} records were deleted`)
-  }).catch(next)
+    .then((number) => {
+      number === 1
+        ? res.status(200).json(req.user)
+        : res.send(`${number} records were deleted`);
+    })
+    .catch(next);
 });
 
-router.get('/:id/posts', (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
+router.get("/:id/posts", validateUserId, async (req, res, next) => {
+  try {
+    const posts = await User.getUserPosts(req.params.id);
+    res.status(200).json(posts);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
